21557139de67ce4e91501275a54b67f124533c15
陈墨 <1973657048@qq.com>
2024-10-27 18:43:22 +0800

    feat：项目配置问题

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-  return <div>hello next</div>;
+  return (
+    <div>
+      <div className="flex justify-center text-center">
+      We are a Candy Wholesale, support OEM/ODM The website currently only
+          shows some products picture，if you are interest in product, welcome
+          to contact us
+      </div>
+    </div>
+  );
 }
