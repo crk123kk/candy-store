2e3c9dcd9786ad65879bd59366bf110acf09ad95
陈墨 <1973657048@qq.com>
2024-10-30 18:08:57 +0800

    refactor：重构项目

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
+import Header from "./ui/page/header";
 import GoodList from "./ui/store/goods-list";
 
 export default function Page() {
   return (
     <div>
-      <div className="text-center bg-pink-bg text-white">
-        <h1 className="leading-7">We are a Candy Wholesale, support OEM/ODM</h1>
-        <h2 className="leading-8">
-          The website currently only shows some products picture，if you are
-          interest in product, welcome to contact us
-        </h2>
+      <div>
+        <Header />
       </div>
       <div>
         <input></input>
@@ -17,6 +14,7 @@ export default function Page() {
       <div>
         <GoodList></GoodList>
       </div>
+      <div></div>
     </div>
   );
 }
---
+++ app/ui/page/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="text-center bg-pink-bg text-white">
+      <h1 className="leading-7">We are a Candy Wholesale, support OEM/ODM</h1>
+      <h2 className="leading-8">
+        The website currently only shows some products picture，if you are
+        interest in product, welcome to contact us
+      </h2>
+    </div>
+  );
+}
