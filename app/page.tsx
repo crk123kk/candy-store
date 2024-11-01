2ac2e1278779f2729fe22347b210f41fe3ad8916
陈墨 <1973657048@qq.com>
2024-11-01 08:42:33 +0800

    feat: add menu list

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Footer from "./ui/page/footer";
 import Header from "./ui/page/header";
+import MenuList from "./ui/page/menuList";
 import GoodList from "./ui/store/goods-list";
 
 export default function Page() {
@@ -11,7 +12,9 @@ export default function Page() {
       <div>
         <input></input>
       </div>
-      <div>menu</div>
+      <div>
+        <MenuList />
+      </div>
       <div>
         <GoodList></GoodList>
       </div>
---
+++ app/ui/page/menuList.tsx
@@ -0,0 +1,4 @@
+export default function MenuList() {
+    return <div>MenuList</div>;
+  }
+  
